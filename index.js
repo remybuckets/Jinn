40e49205268b6d1d8f5fe9a7c5fd069e8e3effed
Jeremy Holguin <jrems@Jeremys-MacBook-Air.local>
2024-08-08 13:38:38 +0200

    Update-2

--- index.js
+++ index.js
@@ -12,3 +12,11 @@ const hiddenElements = document.querySelectorAll('.fades');
 const longHiddenElements = document.querySelectorAll('.lfades')
 hiddenElements.forEach((el) => observer.observe(el));
 longHiddenElements.forEach((el) => observer.observe(el));
+
+const navLinks = document.querySelectorAll('.nav-item')
+const menuToggle = document.getElementById('navbarSupportedContent')
+const bsCollapse = new bootstrap.Collapse(menuToggle, {toggle:false})
+
+navLinks.forEach((l) => {
+    l.addEventListener('click', () => { bsCollapse.toggle() })
+})
